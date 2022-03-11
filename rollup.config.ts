e41fff9854f0d04f43d28dfb608fa1ee73c670f6
BackRunner <dev@backrunner.top>
2022-03-12 03:26:09 +0800

    feat: add filesize plugin

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,8 +1,9 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
-import json from '@rollup/plugin-json';
+import filesize from 'rollup-plugin-filesize';
 import pkg from './package.json';
 
 export default {
@@ -15,5 +16,5 @@ export default {
   watch: {
     include: 'src/**',
   },
-  plugins: [json(), typescript({ useTsconfigDeclarationDir: true }), commonjs(), resolve(), sourceMaps()],
+  plugins: [json(), typescript({ useTsconfigDeclarationDir: true }), commonjs(), resolve(), sourceMaps(), filesize()],
 };
