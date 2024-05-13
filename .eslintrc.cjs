c5f781dd45a17686c19a30fe1044d07d1c8ba706
BackRunner <dev@backrunner.top>
2024-05-13 23:37:20 +0800

    fix: wrong eslintrc implementation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  'import/order': [
-    'warn',
-    {
-      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-      'newlines-between': 'always',
-      alphabetize: { order: 'asc', caseInsensitive: true },
-    },
-  ],
+  plugins: ['import'],
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
 };
